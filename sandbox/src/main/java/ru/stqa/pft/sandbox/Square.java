310df840e935cd4f0e965d2c5860e4a1074660a7
natalya83 <natalya83@inbox.lv>
2016-10-21 23:43:58 +0300

    Created class Square

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by User on 21.10.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square (double l) {
+    this.l = l;
+  }
+  public double area (){
+    return this.l * this.l;
+  }
+}
