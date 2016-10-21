a79c9e13e5c9a6aa95303633255e718ce31822a3
natalya83 <natalya83@inbox.lv>
2016-10-21 22:47:14 +0300

    Added few functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] agrs) {
-    System.out.println("Hello,world!");
+    hello ("world");
+    hello("user");
+    hello("Nat");
+
+    double l = 5;
+    System.out.println("Area of a square with a side " + l + " = " + area (l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of a rectangle with sides " + a + " and " + b + " = " +area (a, b) );
+
   }
+
+  public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double len){
+    return len * len;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+  }
+
 
 }
