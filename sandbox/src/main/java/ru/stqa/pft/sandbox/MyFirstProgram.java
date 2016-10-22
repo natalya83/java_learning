cd0d9671bfa9505a017ca58b46a314de561e36d3
natalya83 <natalya83@inbox.lv>
2016-10-22 19:20:29 +0300

    Turned function into method in Point class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,11 +13,8 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Area of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
-    double x1 = 2;
-    double x2 = 9;
-    double y1 = 2;
-    double y2 = 7;
-    System.out.println("Distance between the dots = " + distance(x1, x2, y1, y2));
+    Point p = new Point (2, -9, 2, 13);
+    System.out.println("Distance between the dots with coordinate = " + p.x1 + "," + p.y1 + " and " + p.x2 + ","+ p.y2 + " = " + p.distance());
 
   }
 
@@ -26,8 +23,6 @@ public class MyFirstProgram {
 
   }
 
-  public static double distance (double x1, double x2, double y1, double y2){
-    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-  }
+
 
 }
