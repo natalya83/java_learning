9509d3eec2ea51aff8cfb653dc2c5f126fbe357a
natalya83 <natalya83@inbox.lv>
2016-10-22 17:25:06 +0300

    Created function to calculate the distance between two dots

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] agrs) {
-    hello ("world");
+    hello("world");
     hello("user");
     hello("Nat");
 
@@ -13,11 +13,21 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Area of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
+    double x1 = 2;
+    double x2 = 9;
+    double y1 = 2;
+    double y2 = 7;
+    System.out.println("Distance between the dots = " + distance(x1, x2, y1, y2));
+
   }
 
-  public static void hello (String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
   }
 
+  public static double distance (double x1, double x2, double y1, double y2){
+    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+  }
 
 }
