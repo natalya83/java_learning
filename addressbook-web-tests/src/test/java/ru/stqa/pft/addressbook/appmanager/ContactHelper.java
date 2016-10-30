ff8f659721e94e4c2c89bb53c12a7b95f8f150bf
natalya83 <natalya83@inbox.lv>
2016-10-30 22:20:27 +0200

    Added new methods to ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -28,4 +28,24 @@ public class ContactHelper extends HelperBase{
   public void initContactCreation() {
       click(By.linkText("add new"));
   }
+
+  public void selectContact() {
+    click(By.id("20"));
+  }
+
+  public void deleteSelectedContacts() {
+    click(By.xpath("//div[@id='content']/form[2]/div[2]input"));
+  }
+
+  public void initContactModification() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+  }
+
+  public void returnToHomePage() {
+    click(By.linkText("home page"));
+  }
 }
