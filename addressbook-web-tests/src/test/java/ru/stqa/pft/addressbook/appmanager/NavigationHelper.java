cb8342ffcfa1c0cf6573488061d7f7ab2f6731d7
natalya83 <natalya83@inbox.lv>
2016-10-30 20:39:49 +0200

    NavigationHelper class now extends HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,17 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by User on 28.10.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super (wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
-  public void gotoHomePage() {wd.findElement(By.linkText("home")).click();
+  public void gotoHomePage() {click(By.linkText("home"));
   }
 }
