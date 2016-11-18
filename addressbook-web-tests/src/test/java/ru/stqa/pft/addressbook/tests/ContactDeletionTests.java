95a89f355c41dc543006bc846eea6d8a62a6fb10
natalya83 <natalya83@inbox.lv>
2016-11-18 11:45:14 +0200

    Added pre-condition for contact existance to ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
  * Created by User on 30.10.2016.
@@ -10,6 +11,9 @@ public class ContactDeletionTests extends TestBase {
   @Test
   public void testContactDeletion (){
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Natalja", "Kastrica", "26183677", "natalya83@inbox.lv", "test1"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().acceptAlert();
